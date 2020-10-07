04dd7ec821a95753dd2bf91e8626fe8afb67c2cf
Noe <75214649@continental.edu.pe>
2020-10-07 12:38:16 -0500

    Noe

--- resources/views/seguridad.blade.php
+++ resources/views/seguridad.blade.php
@@ -5,136 +5,56 @@
 @endsection
 @section('content')
 
-<div class="card">
-    <div class="card-header">
-        <h3 class="card-title">Detalles de seguridad</h3>
-    </div>
+        <!-- START ALERTS AND CALLOUTS -->
+        <h5 class="mt-4 mb-2">Detalle delitos</h5>
 
-<div class="col-md-3">
-    <div class="card bg-warning">
-    <div class="card-header">
-    <h3 class="card-title">Delito más frecuente</h3>
-
-    <div class="card-tools">
-    <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
-    </button>
-    </div>
-    <!-- /.card-tools -->
-    </div>
-    <!-- /.card-header -->
-    <div class="card-body">
-    inf base de datos
-    </div>
-    <!-- /.card-body -->
-    </div>
-    <!-- /.card -->
-</div>
-
-<div class="col-md-3">
-    <div class="card bg-warning">
-    <div class="card-header">
-    <h3 class="card-title">Zonas más peligrosa</h3>
-
-    <div class="card-tools">
-    <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
-    </button>
-    </div>
-    <!-- /.card-tools -->
-    </div>
-    <!-- /.card-header -->
-    <div class="card-body">
-    inf base de datos
-    </div>
-    <!-- /.card-body -->
-    </div>
-    <!-- /.card -->
-</div>
-
-<div class="col-md-3">
-    <div class="card bg-warning">
-    <div class="card-header">
-    <h3 class="card-title">Cantidad delitos captados</h3>
-
-    <div class="card-tools">
-    <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
-    </button>
-    </div>
-    <!-- /.card-tools -->
-    </div>
-    <!-- /.card-header -->
-    <div class="card-body">
-    inf base de datos
-    </div>
-    <!-- /.card-body -->
-    </div>
-    <!-- /.card -->
-</div>
-
-<div class="form-group">
-    <label>Seleccione acto delictivo</label>
-    <select class="form-control">
-    <option>Hurto</option>
-    <option>Robo</option>
-    <option>Abigeato</option>
-    <option>Asesinato</option>
-    </select>
-</div>
-
-
-<body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
-<section class="content">        
-<!-- Main row -->
         <div class="row">
-          <!-- Left col -->
-          <div class="col-md-8">
-            <!-- MAP & BOX PANE -->
-            <div class="card">
+          <div class="col-md-6">
+            <div class="card card-default">
               <div class="card-header">
-                <h3 class="card-title">US-Visitors Report</h3>
-
-                <div class="card-tools">
-                  <button type="button" class="btn btn-tool" data-card-widget="collapse">
-                    <i class="fas fa-minus"></i>
-                  </button>
-                  <button type="button" class="btn btn-tool" data-card-widget="remove">
-                    <i class="fas fa-times"></i>
-                  </button>
-                </div>
+                <h3 class="card-title">
+                  <i class="fas fa-exclamation-triangle"></i>
+                  Alertas
+                </h3>
               </div>
               <!-- /.card-header -->
-              <div class="card-body p-0">
-                <div class="d-md-flex">
-                  <div class="p-1 flex-fill" style="overflow: hidden">
-                    <!-- Map will be created here -->
-                    <div id="world-map-markers" style="height: 325px; overflow: hidden">
-                      <div class="map"></div>
-                    </div>
-                  </div>
-                  <div class="card-pane-right bg-success pt-2 pb-2 pl-4 pr-4">
-                    <div class="description-block mb-4">
-                      <div class="sparkbar pad" data-color="#fff">90,70,90,70,75,80,70</div>
-                      <h5 class="description-header">8390</h5>
-                      <span class="description-text">Visits</span>
-                    </div>
-                    <!-- /.description-block -->
-                    <div class="description-block mb-4">
-                      <div class="sparkbar pad" data-color="#fff">90,50,90,70,61,83,63</div>
-                      <h5 class="description-header">30%</h5>
-                      <span class="description-text">Referrals</span>
-                    </div>
-                    <!-- /.description-block -->
-                    <div class="description-block">
-                      <div class="sparkbar pad" data-color="#fff">90,50,90,70,61,83,63</div>
-                      <h5 class="description-header">70%</h5>
-                      <span class="description-text">Organic</span>
-                    </div>
-                    <!-- /.description-block -->
-                  </div><!-- /.card-pane-right -->
-                </div><!-- /.d-md-flex -->
+              <div class="card-body">
+                <div class="alert alert-info alert-dismissible">
+                  <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                  <h5><i class="icon fas fa-info"></i>Delito más frecuente!</h5>
+                  Info alert preview. This alert is dismissable.
+                </div>
+                <div class="alert alert-warning alert-dismissible">
+                  <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                  <h5><i class="icon fas fa-exclamation-triangle"></i>Zona más peligrosa!</h5>
+                  Warning alert preview. This alert is dismissable.
+                </div>
+                <div class="alert alert-success alert-dismissible">
+                  <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                  <h5><i class="icon fas fa-check"></i> Cantidad delitos captados!</h5>
+                  Success alert preview. This alert is dismissable.
+                </div>
               </div>
               <!-- /.card-body -->
             </div>
             <!-- /.card -->
+          </div>
+          <!-- /.col -->
+          <div class="col-sm-6">
+                      <!-- select -->
+                      <div class="form-group">
+                        <label>Select</label>
+                        <select class="form-control">
+                          <option>option 1</option>
+                          <option>option 2</option>
+                          <option>option 3</option>
+                          <option>option 4</option>
+                          <option>option 5</option>
+                        </select>
+                      </div>
+                    </div>
+
+        </div>
 
 @endsection
 
