f9dc2941215b72b72aa9b4fae44bc17246260218
Cristian Castro <texvnonline@gmail.com>
2020-10-07 18:58:29 -0500

    Cristian

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -102,7 +102,7 @@
                             </a>
                             <ul class="nav nav-treeview">
                                 <li class="nav-item">
-                                    <a href="" class="nav-link">
+                                    <a href="{{route('parks.dashboard')}}" class="nav-link">
                                         <i class="fas fa-tachometer-alt nav-icon"></i>
                                         <p>Panel</p>
                                     </a>
